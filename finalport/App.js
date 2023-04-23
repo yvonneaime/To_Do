48f06a51b3642ed3c3c353e8eb0931bb475a43b5
Yvonne Aime <yvonneaime0@gmail.com>
2023-04-23 19:33:34 -0400

    setup

---
+++ finalport/App.js
@@ -0,0 +1,28 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
+import FontAwesome from '@expo/vector-icons/FontAwesome'
+
+const Tab = createBottomTabNavigator()
+
+async function cacheFonts(fonts) {
+  return fonts.map(async (font) => await FontAwesome.loadAsync(font))
+}
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
