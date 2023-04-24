45438a951268ae0c11f6ba2ab902e05ecc457e90
Yvonne Aime <yvonneaime0@gmail.com>
2023-04-23 20:26:51 -0400

    update

--- finalport/App.js
+++ finalport/App.js
@@ -1,21 +1,69 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
 import FontAwesome from '@expo/vector-icons/FontAwesome'
+import { useEffect, useState } from 'react'
+import { NavigationContainer } from "@react-navigation/native"
+import AsyncStorage from "@react-native-async-storage/async-storage"
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
 const Tab = createBottomTabNavigator()
 
 async function cacheFonts(fonts) {
   return fonts.map(async (font) => await FontAwesome.loadAsync(font))
 }
+let newTasks = [
+  {description: "Task 1", completed: true, key:1, relatedTasks: [2]},
+  {description: "Task 2", completed: true, key:2}
+]
+
+const Stack = createNativeStackNavigator()
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <NavigationContainer>
+      <Tab.Navigator>
+      <Tab.Screen
+        name="Home"
+        component={TodoHomeScreen}
+        options={{ headerShown: false }}
+        />
+      <Tab.Screen name="Settings" component={SettingsScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  )
+}
+function SettingsScreen() {
+  return <Text>Settings Screen</Text>
+}
+
+function TodoHomeScreen() {
+  let [tasks, setTasks] = useState(newTasks)
+  useEffect(() => {
+    async function getValue() {
+      const value = await AsyncStorage.getItem("@tasks")
+      if (value === null) {
+        console.log(
+          "Storing serialized tasks" + JSON.stringify(tasks)
+        )
+        await AsyncStorage.setItem("@tasks", JSON.stringify(tasks))
+      } else {
+        let parsedValue = JSON.parse(value)
+        console.log("Retrieving serialized tasks")
+        console.log(parsedValue)
+        setTasks(JSON.parse(value))
+      }
+    }
+    getValue()
+  }, [])
+  return <StackNavigator>
+    <Stack.Screen name="To Do List">
+      {(props) => (
+        <TodoHomeScreen
+      )}
+    </Stack.Screen>
+
+  </StackNavigator>
 }
 
 const styles = StyleSheet.create({
